284c4462941b553f5ac1cc4d54ab28a12c86aaeb
David Kennedy <David.Kennedy@umassmed.edu>
2020-05-22 06:17:34 -0400

    Progress in speedkwyk design
    end of day

--- speedkwyk_local.sh
+++ speedkwyk_local.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -eu
 
-echo "abcd-kwyker_awslocal: Running kwyk container on EC2 resource on ABCD S3 file on aws - local controller"
+echo "speedkwyk_local: Running kwyk container on EC2 resource on test case - local controller"
+echo "For NITRC Speed tests"
 
 # This is the main script, for local management of the analysis process, that, given
-# the S3 path to an anatomic case, manages its fetch, kwyk run, and 'post' to
+# an instance type, manages a kwyk run for timing, and 'post' of results to
 # the named ReproNim S3 results location
 # At the moment it is expecting to run via the AWS-RunShellScript System 
 # Management functions, as user = root
@@ -12,38 +13,34 @@ echo "abcd-kwyker_awslocal: Running kwyk container on EC2 resource on ABCD S3 fi
 # Check usage, 2 argument expected.
 if [ "$#" -ne 2 ]; then
   echo "Illegal number of parameters provided"
-  echo "Expected usage: abcd-kwyker_local.sh S3_file_name outputdirectoryname" 
+  echo "Expected usage: speedkwyk_local.sh AWSEC2_instance_type outputdirectoryname" 
   exit 10
 fi
 
 # setup variables
-s3filenam=$1
+itype=$1
 basenam=$2
 
-# Identify AWS EC2 instance ID
+# Identify AWS EC2 instance ID 
+# prepare this instance to have the test dataset available, assumed
 IID=i-0e8750dd1c7e02355
 
-remotescript=abcd-kwyker_aws.sh
-#localscriptpath=~/bin
+# Script to run on the remoth instance
+# Path 
 localscriptpath=~/GitHub/CloudComputeExample
+# Script name
+remotescript=speedkwyk_aws.sh
+
+# Location for script on remote instance
 remotescriptpath=~ubuntu/bin
 
 # We are using aws 'profile' for credential management.
 # We expect the .aws/configuration file to be pushed from your local system (i.e. here)
 
-# TODO insert a checker to see if you have a valid and currently active 
-# session token, and to provide appropriate instructions if you don't...
-# There are two points here: 1) we want the 'on aws' activities to be completly
-# 'hands-off' for efficiency purposes; and 2) we'd like to not have the user 
-# entering their NDA authorization very often.
+# Make sure instance is stopped
 
-# IF creds fail, then do the following:
-#PrepCreds (if needed) will result in an .aws/credentials file
-#Get token content into /Users/davidkennedy/downloadmanager/tempcred.txt
-#docker run -it --rm -v $NDAdir:/data debian /data/debian_generate_token.sh $NDAuser $NDApasswd
-
-#Patch AWS Creds
-#~/bin/prep_creds.sh $NDAuser
+# Set Instance type
+aws ec2 modify-instance-attribute --instance-id $IID --instance-type "{\"Value\": \"$itype\"}"
 
 # Start Instance
 echo "Launching and waiting"
@@ -57,11 +54,13 @@ IP=`aws ec2 describe-instances --instance-ids $IID --query 'Reservations[*].Inst
 echo "Found IP as $IP"
 
 # Push stuff to instance
-# Push Creds to instance
-scp -o StrictHostKeyChecking=no -i ~/DNK_CRNC.pem ~/.aws/credentials ubuntu@${IP}:~/.aws/.
+# Since this speed test is always the same details for run, let's assume we can 
+# pre-load all the relevant stuff there. What stuff, you ask:
+# The creds
+# scp -o StrictHostKeyChecking=no -i ~/DNK_CRNC.pem ~/.aws/credentials ubuntu@${IP}:~/.aws/.
 
-# Push script to instance
-scp -i ~/DNK_CRNC.pem ${localscriptpath}/$remotescript ubuntu@${IP}:${remotescriptpath}/.
+# The script
+# scp -i ~/DNK_CRNC.pem ${localscriptpath}/$remotescript ubuntu@${IP}:${remotescriptpath}/.
 
 # Launch process on instance
 
@@ -70,7 +69,7 @@ echo "commands=${remotescriptpath}/$remotescript $s3filenam $basenam"
 aws ssm send-command --document-name "AWS-RunShellScript" --document-version "1" \
  --instance-ids "$IID" --parameters "commands=${remotescriptpath}/$remotescript $s3filenam $basenam" \
  --timeout-seconds "600" --max-concurrency "50" --max-errors "0" \
- --output-s3-bucket-name "kwyktest" --region us-east-1 --profile reprodnk
+ --output-s3-bucket-name "speedkwyk" --region us-east-1 --profile reprodnk
 
 # the end
 echo "Your process has been launched.  The instance will stop itself upon "
